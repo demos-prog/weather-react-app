87c54fbaa0b12702e9c823043bb29a3e73b43e49
demos <demosne@gmail.com>
2021-10-06 20:14:57 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./null_styles.css";
 import "./App.css";
-import * as weather from 'openweather-apis'
+import * as weather from "openweather-apis";
 import { useState } from "react";
 
 const apiKey = "f70ad6074fa13b00157e997a81cc4525";
@@ -8,17 +8,13 @@ const apiKey = "f70ad6074fa13b00157e997a81cc4525";
 function App() {
   const [city, setCity] = useState("Minsk");
 
-  weather.setLang('en');
-	weather.setCity(city);
- 	weather.setUnits('metric');
- 	weather.setAPPID(apiKey);
+  weather.setLang("en");
+  weather.setCity(city);
+  weather.setUnits("metric");
+  weather.setAPPID(apiKey);
 
-
-  weather.getTemperature(function(err, temp){
-		console.log(temp);
-	});
-  weather.getPressure(function(err, pres){
-		console.log(pres);
+  weather.getAllWeather(function(err, smart){
+		console.log(smart);
 	});
 
   return <div></div>;
