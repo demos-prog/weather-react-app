1ba6a2bb37ec3173c6e6f8b81377636eefcbf50a
demos <demosne@gmail.com>
2021-10-07 21:29:03 +0300

    css

--- src/components/weatherToday.jsx
+++ src/components/weatherToday.jsx
@@ -1,20 +1,15 @@
 import "./WeatherToday.css";
 
-export default function WeatherToday({
-  city,
-  temp,
-  humidity,
-  description,
-}) {
- 
+export default function WeatherToday({ city, temp, humidity, description }) {
   return (
     <div className="wrapperWeatherToday">
       <div className="contetntWeatherToday">
+        <div>Weather today</div>
         <div className="descr">
           <h1>{city}</h1>
-          <h2>{description}</h2>
         </div>
         <div>
+          <h2>{description}</h2>
           <div>temperature: {temp} &deg;C</div>
           <div>humidity: {humidity} RH</div>
         </div>
